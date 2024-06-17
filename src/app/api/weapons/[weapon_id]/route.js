5264b1c973cf6c3c8bb202d4dbbb6ef6814d3194
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 20:53:53 +0100

    Implemented /api/weapons/[weapon_id]

--- src/app/api/weapons/[weapon_id]/route.js
+++ src/app/api/weapons/[weapon_id]/route.js
@@ -1,11 +1,18 @@
 import {NextResponse} from "next/server";
-
-// TODO: Implement the route
+import {getWeapon} from "@lib/Weapons";
 
 // Returns data for specific weapon
 
-export async function GET(request) {
-    return NextResponse.json({
-        message: "Not Implemented"
-    }, {status: 501});
+export async function GET(request, {params}) {
+    const weapon_id = params.weapon_id.toLowerCase();
+    const weapon = getWeapon(weapon_id);
+    if (weapon) {
+        return NextResponse.json({
+            weapon: weapon
+        }, {status: 200});
+    } else {
+        return NextResponse.json({
+            message: `Weapon with id ${weapon_id} not found.`
+        }, {status: 404});
+    }
 }
