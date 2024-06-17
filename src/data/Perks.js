873cbbc097f64ddae09b3ef9ed96b18bab473834
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 21:16:26 +0100

    Added test data

--- src/data/Perks.js
+++ src/data/Perks.js
@@ -1,1 +1,27 @@
-export const Perks = [];
+export const Perks = [
+    {
+        "id": "test_perk_1",
+        "name": "Test Perk 1",
+        "description": "This is a test perk.",
+    },
+    {
+        "id": "test_perk_2",
+        "name": "Test Perk 2",
+        "description": "This is a test perk.",
+    },
+    {
+        "id": "test_perk_3",
+        "name": "Test Perk 3",
+        "description": "This is a test perk.",
+    },
+    {
+        "id": "test_perk_4",
+        "name": "Test Perk 4",
+        "description": "This is a test perk.",
+    },
+    {
+        "id": "test_origin_1",
+        "name": "Test Origin 1",
+        "description": "This is a test origin perk.",
+    }
+];
--- src/data/Weapons.js
+++ src/data/Weapons.js
@@ -1,1 +1,21 @@
-export const Weapons = [];
+export const Weapons = [
+    {
+        "id": "test",
+        "name": "Test Weapon",
+        "type": "Hand Cannon",
+        "rarity": "Legendary",
+        "perks": {
+            "1": [
+                "test_perk_1",
+                "test_perk_2"
+            ],
+            "2": [
+                "test_perk_3",
+                "test_perk_4"
+            ],
+            "origin": [
+                "test_origin_1"
+            ]
+        }
+    }
+];
