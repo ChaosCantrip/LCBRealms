398cdd4fb93d273fe55754793ac456d6f40f851f
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 20:51:21 +0100

    Added getPerk()

---
+++ src/lib/Perks.js
@@ -0,0 +1,10 @@
+import { Perks } from "@data/Perks";
+
+export function getPerk(perk_id) {
+    for (let i = 0; i < Perks.length; i++) {
+        if (Perks[i].id === perk_id) {
+            return Perks[i];
+        }
+    }
+    return null;
+}
