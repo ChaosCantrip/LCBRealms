4d314034641a93b6fe59f5def976a04fd446d280
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 20:54:56 +0100

    Implemented /api/perks/[perk_id]

--- src/app/api/perks/[perk_id]/route.js
+++ src/app/api/perks/[perk_id]/route.js
@@ -1,11 +1,18 @@
 import {NextResponse} from "next/server";
-
-// TODO: Implement the route
+import {getPerk} from "@lib/Perks";
 
 // Returns data for specific perk
 
-export async function GET(request) {
-    return NextResponse.json({
-        message: "Not Implemented"
-    }, {status: 501});
+export async function GET(request, {params}) {
+    const perk_id = params.perk_id.toLowerCase();
+    const perk = getPerk(perk_id);
+    if (perk) {
+        return NextResponse.json({
+            perk: perk
+        }, {status: 200});
+    } else {
+        return NextResponse.json({
+            message: `Perk with id ${perk_id} not found.`
+        }, {status: 404});
+    }
 }
