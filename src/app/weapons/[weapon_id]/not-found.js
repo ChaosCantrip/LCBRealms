9181cd5cc947f18bd3d308f7bcb25b200dbd03a9
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 21:06:53 +0100

    Added basic weapon_id not-found page

---
+++ src/app/weapons/[weapon_id]/not-found.js
@@ -0,0 +1,8 @@
+export default function WeaponNotFound() {
+    return (
+        <div>
+            <h1>Weapon Not Found</h1>
+            <p>The weapon you are looking for does not exist.</p>
+        </div>
+    );
+}
