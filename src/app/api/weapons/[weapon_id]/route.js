086025b60dde0007eadb8bd3b47a9ddc62b62243
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 19:44:06 +0100

    Added /api/weapons/[weapon_id]

---
+++ src/app/api/weapons/[weapon_id]/route.js
@@ -0,0 +1,11 @@
+import {NextResponse} from "next/server";
+
+// TODO: Implement the route
+
+// Returns data for specific weapon
+
+export async function GET(request) {
+    return NextResponse.json({
+        message: "Not Implemented"
+    }, {status: 501});
+}
