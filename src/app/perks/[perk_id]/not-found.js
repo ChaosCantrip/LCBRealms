63c0a38ab225ff2e9e4e6c8aec672b81d0278cad
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 21:08:47 +0100

    Added basic perk_id not-found page

---
+++ src/app/perks/[perk_id]/not-found.js
@@ -0,0 +1,8 @@
+export default function PerkNotFound() {
+    return (
+        <div>
+            <h1>Perk Not Found</h1>
+            <p>The perk you are looking for does not exist.</p>
+        </div>
+    );
+}
