361abfb06b1ea2b82e6b9c62fd07000062ac1d35
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 21:35:19 +0100

    Added basic weapon details page

--- src/app/weapons/[weapon_id]/page.js
+++ src/app/weapons/[weapon_id]/page.js
@@ -1,9 +1,46 @@
-export default function WeaponPage({ params }) {
+import {Suspense} from "react";
+
+export default async function WeaponPage({ params }) {
     const weapon_id = params.weapon_id;
+    const weapon_response = await fetch(process.env.URL + '/api/weapons/' + weapon_id);
+    const weapon = await weapon_response.json();
     return (
         <div>
-            <h1>Weapon Page</h1>
-            <p>This is the weapon page for weapon ID {weapon_id}.</p>
+            <h1>{weapon.name}</h1>
+            <p>{weapon.rarity} {weapon.type}</p>
+            <h2>Perks</h2>
+            <div>
+                {Object.entries(weapon.perks).map(([column, perks]) => (
+                    <div key={column}>
+                        <h3>{column}</h3>
+                        {perks.map(perk_id => (
+                            <Suspense key={perk_id} fallback={<LoadingPerk perk_id={perk_id} />}>
+                                <Perk perk_id={perk_id} />
+                            </Suspense>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+async function Perk({perk_id}) {
+    const perk_response = await fetch(process.env.URL + '/api/perks/' + perk_id);
+    const perk = await perk_response.json();
+    return (
+        <div>
+            <h4>{perk.name}</h4>
+            <p>{perk.description}</p>
+        </div>
+    );
+}
+
+function LoadingPerk({perk_id}) {
+    return (
+        <div>
+            <h4>Loading...</h4>
+            <p>{perk_id}</p>
         </div>
     );
 }
