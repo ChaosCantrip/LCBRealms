708f84c2a034366f5d34f845d1ff6c2eb8152a03
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 19:44:59 +0100

    Added /api/perks/[perk_id]

---
+++ src/app/api/perks/[perk_id]/route.js
@@ -0,0 +1,11 @@
+import {NextResponse} from "next/server";
+
+// TODO: Implement the route
+
+// Returns data for specific perk
+
+export async function GET(request) {
+    return NextResponse.json({
+        message: "Not Implemented"
+    }, {status: 501});
+}
