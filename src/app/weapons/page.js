dacf3cab4b2d59e36a795c7ff61086f4e13598b9
ChaosCantrip <dev@chaoscantrip.com>
2024-06-17 21:23:05 +0100

    Updated weapons page to display all weapons

--- src/app/weapons/page.js
+++ src/app/weapons/page.js
@@ -1,8 +1,21 @@
-export default function WeaponsPage() {
+import Link from "next/link";
+
+export default async function WeaponsPage() {
+    const weapons_response = await fetch(process.env.URL + '/api/weapons');
+    const weapons = await weapons_response.json();
     return (
         <div>
-            <h1>Weapons Page</h1>
-            <p>This is the weapons page.</p>
+            <h1>Weapons</h1>
+            <ul>
+                {weapons.map(weapon => (
+                    <li key={weapon.id}>
+                        <Link href={`/weapons/${weapon.id}`}>
+                            <p>{weapon.name}</p>
+                            <p>{weapon.rarity} {weapon.type}</p>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
