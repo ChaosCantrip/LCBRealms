887e01141d433ca8d90e73747c73fce8fe213477
ChaosCantrip <dev@chaoscantrip.com>
2024-06-18 12:16:23 +0100

    Added tooltips and handling of unknown perks and weapons

--- src/app/weapons/[weapon_id]/page.js
+++ src/app/weapons/[weapon_id]/page.js
@@ -3,10 +3,14 @@ import {Suspense} from "react";
 import minecraft from "@styles/modules/minecraft_text.module.css";
 import styles from "./custom.module.css";
 import tooltip from "@styles/modules/tooltip.module.css";
+import {notFound} from "next/navigation";
 
 export default async function WeaponPage({ params }) {
     const weapon_id = params.weapon_id;
     const weapon_response = await fetch(process.env.URL + '/api/weapons/' + weapon_id);
+    if (!weapon_response.ok) {
+        notFound();
+    }
     const weapon = await weapon_response.json();
     return (
         <div>
@@ -31,6 +35,15 @@ export default async function WeaponPage({ params }) {
 
 async function Perk({perk_id}) {
     const perk_response = await fetch(process.env.URL + '/api/perks/' + perk_id);
+    if (!perk_response.ok) {
+        return (
+            <div className={`${styles.perk} ${tooltip.tooltip}`}>
+                <h4 className={`${minecraft.bold} ${minecraft.dark_red} ${minecraft.shadow}`}>Unknown Perk</h4>
+                <p>{perk_id}</p>
+            </div>
+        );
+    }
+
     const perk = await perk_response.json();
     return (
         <div className={`${styles.perk} ${tooltip.tooltip}`}>
