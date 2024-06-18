c2f9e5d3dd602be8a5ad0f45335ebf7e9a589723
ChaosCantrip <dev@chaoscantrip.com>
2024-06-18 12:13:58 +0100

    Updated weapons data

--- src/data/Weapons.js
+++ src/data/Weapons.js
@@ -1,20 +1,64 @@
 export const Weapons = [
     {
-        "id": "test",
-        "name": "Test Weapon",
+        "id": "flight_of_the_bird",
+        "name": "Flight of the Bird",
         "type": "Hand Cannon",
         "rarity": "Legendary",
         "perks": {
             "Column 1": [
-                "test_perk_1",
-                "test_perk_2"
+                "extended_mag",
+                "fast_mag",
+                "light_mag"
             ],
             "Column 2": [
-                "test_perk_3",
-                "test_perk_4"
+                "headsplitter",
+                "precision_instrument",
+                "backstab"
             ],
             "Origin Trait": [
-                "test_origin_1"
+                "rooting_rounds"
+            ]
+        }
+    },
+    {
+        "id": "forest_fire",
+        "name": "Forest Fire",
+        "type": "Submachine Gun",
+        "rarity": "Legendary",
+        "perks": {
+            "Column 1": [
+                "heavy_mag",
+                "extended_mag",
+                "light_mag"
+            ],
+            "Column 2": [
+                "bane_of_arthropods",
+                "headsplitter",
+                "smite"
+            ],
+            "Origin Trait": [
+                "rooting_rounds"
+            ]
+        }
+    },
+    {
+        "id": "thundering_boom",
+        "name": "Thundering Boom",
+        "type": "Sniper Rifle",
+        "rarity": "Legendary",
+        "perks": {
+            "Column 1": [
+                "origin_enhancement",
+                "fast_mag",
+                "heavy_mag"
+            ],
+            "Column 2": [
+                "headsplitter",
+                "precision_instrument",
+                "backstab"
+            ],
+            "Origin Trait": [
+                "rooting_rounds"
             ]
         }
     }
