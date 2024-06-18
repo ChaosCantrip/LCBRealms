78a409f90f8327bb0dbe94b148ce6d6a35380a99
ChaosCantrip <dev@chaoscantrip.com>
2024-06-18 12:04:32 +0100

    Imrpoved styling on weapon page

--- src/app/weapons/[weapon_id]/page.js
+++ src/app/weapons/[weapon_id]/page.js
@@ -1,18 +1,22 @@
 import {Suspense} from "react";
 
+import minecraft from "@styles/modules/minecraft_text.module.css";
+import styles from "./custom.module.css";
+import tooltip from "@styles/modules/tooltip.module.css";
+
 export default async function WeaponPage({ params }) {
     const weapon_id = params.weapon_id;
     const weapon_response = await fetch(process.env.URL + '/api/weapons/' + weapon_id);
     const weapon = await weapon_response.json();
     return (
         <div>
-            <h1>{weapon.name}</h1>
+            <h1 className={`${minecraft.bold} ${minecraft.white} ${minecraft.shadow}`}>{weapon.name}</h1>
             <p>{weapon.rarity} {weapon.type}</p>
             <h2>Perks</h2>
-            <div>
+            <div className={styles.perks_wrapper}>
                 {Object.entries(weapon.perks).map(([column, perks]) => (
-                    <div key={column}>
-                        <h3>{column}</h3>
+                    <div key={column} className={styles.perks_column}>
+                        <h3 className={styles.column_name}>{column}</h3>
                         {perks.map(perk_id => (
                             <Suspense key={perk_id} fallback={<LoadingPerk perk_id={perk_id} />}>
                                 <Perk perk_id={perk_id} />
@@ -29,16 +33,16 @@ async function Perk({perk_id}) {
     const perk_response = await fetch(process.env.URL + '/api/perks/' + perk_id);
     const perk = await perk_response.json();
     return (
-        <div>
-            <h4>{perk.name}</h4>
-            <p>{perk.description}</p>
+        <div className={`${styles.perk} ${tooltip.tooltip}`}>
+            <h4 className={`${minecraft.bold} ${minecraft.dark_red} ${minecraft.shadow}`}>{perk.name}</h4>
+            <p className={minecraft.gray}>{perk.description}</p>
         </div>
     );
 }
 
 function LoadingPerk({perk_id}) {
     return (
-        <div>
+        <div className={styles.perk}>
             <h4>Loading...</h4>
             <p>{perk_id}</p>
         </div>
