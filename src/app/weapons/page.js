1c26385ecd9b42952d7b35b84c54fb9f737602de
ChaosCantrip <dev@chaoscantrip.com>
2024-06-20 22:10:25 +0100

    Updated weapons page

--- src/app/weapons/page.js
+++ src/app/weapons/page.js
@@ -1,21 +1,30 @@
 import Link from "next/link";
 import {getAllWeapons} from "@lib/Weapons";
 
+import styles from "./custom.module.css";
+import tooltip from "@styles/modules/tooltip.module.css";
+
 export default async function WeaponsPage() {
     const weapons = getAllWeapons();
     return (
         <div>
             <h1>Weapons</h1>
-            <ul>
+            <div className={styles.list}>
                 {weapons.map(weapon => (
-                    <li key={weapon.id}>
-                        <Link href={`/weapons/${weapon.id}`}>
-                            <p>{weapon.name}</p>
-                            <p>{weapon.rarity} {weapon.type}</p>
-                        </Link>
-                    </li>
+                    <Weapon weapon={weapon} key={weapon.id} />
                 ))}
-            </ul>
+            </div>
         </div>
+    );
+}
+
+function Weapon({weapon}) {
+    return (
+        <Link href={`/weapons/${weapon.id}`}>
+            <div className={`${tooltip.tooltip} ${styles.weapon}`}>
+                <p className={styles.weapon_name}>{weapon.name}</p>
+                <p className={styles.weapon_subtitle}>{weapon.rarity} {weapon.type}</p>
+            </div>
+        </Link>
     );
 }
