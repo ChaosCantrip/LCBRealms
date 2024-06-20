920f01480a16c040623c8873c66bd83cda0f3dc3
ChaosCantrip <dev@chaoscantrip.com>
2024-06-20 22:02:50 +0100

    Updated header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,26 @@
 import styles from "./Header.module.css";
+import Link from "next/link";
+
+import tooltip from "@styles/modules/tooltip.module.css";
 
 export default function Header() {
     return (
-        <header className={styles.header}>
-            <h1>Header</h1>
+        <header className={`${styles.header}`}>
+            <div className={styles.title}>
+                <h1>Realms</h1>
+            </div>
+            <div className={styles.nav}>
+                <NavLink href="/">Home</NavLink>
+                <NavLink href="/weapons">Weapons</NavLink>
+            </div>
         </header>
+    );
+}
+
+function NavLink({href, children}) {
+    return (
+        <Link href={href} className={`${styles.nav_button} ${tooltip.tooltip}`}>
+            {children}
+        </Link>
     );
 }
